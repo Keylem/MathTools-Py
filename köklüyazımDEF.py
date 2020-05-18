f19751cee9bf22016108d015fdc298ef2dd64e6d
Keylem <52345143+Keylem@users.noreply.github.com>
2020-05-19 01:41:53 +0300

    Add files via upload
    köklüyazım V2 ( sadece √x'lü stringlerin kabulü eklendi)

---
+++ köklüyazımDEF.py
@@ -0,0 +1,22 @@
+def köklüYazım(inputstring):
+    whilestatemenet = True
+    katmansayısı = 0
+    #
+    if inputstring[0] == "√":
+        return inputstring
+                                                                                #     √
+    elif ("√" in inputstring) == True:
+        while whilestatemenet == True:
+            if inputstring[katmansayısı:katmansayısı + 1] != "√":
+                katmansayısı += 1
+            else:
+                whilestatemenet = False
+        katsayı = float(inputstring[:katmansayısı]) ** 2
+        kök = float(inputstring[katmansayısı + 1 :])
+        inputstring = katsayı * kök
+        
+    else:
+        inputstring = int(inputstring) ** 2
+    return "√{}".format(inputstring)
+
+print(köklüYazım("5"))  # sayıyı girizi (köklü/köksüz)
