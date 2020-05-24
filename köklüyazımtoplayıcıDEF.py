29abe520e84fdf6dd2b735a8ce4f0bbee0b67630
Keylem <52345143+Keylem@users.noreply.github.com>
2020-05-24 17:52:42 +0300

    Update köklüyazımtoplayıcıDEF.py

--- köklüyazımtoplayıcıDEF.py
+++ köklüyazımtoplayıcıDEF.py
@@ -70,7 +70,7 @@ def köklüYazımToplayıcı(inputstring):
         kökiçideğeri = f[2]
 
         if katsayı * kökdışıdeğeri != 0:
-            rawçarpım = rawçarpım * katsayı * kökdışıdeğeri
+            rawçarpım = katsayı ** kökdışıdeğeri * rawçarpım 
         if katsayı * kökiçideğeri != 0:
             rawkök = rawkök * katsayı * kökiçideğeri
     if rawkök == 1:
